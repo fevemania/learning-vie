5bbdaf322e24f7171ee03ad6c9d8e111336624d1
fevemania <brent12052003@gmail.com>
2017-02-04 15:25:19 +0800

    Practical Component Exercise #2: Modal

--- main.js
+++ main.js
@@ -42,7 +42,23 @@ Vue.component('message', {
 	`,
 });
 
+Vue.component('modal', {
+	template: `
+		<div class="modal is-active">
+		  <div class="modal-background"></div>
+		  <div class="modal-content">
+		  	<div class='box'>
+		  		<slot></slot>
+		  	</div>			  
+		  </div>
+		  <button class="modal-close" @click="$emit('close')"></button>
+		</div>
+	`,
+});
 
 new Vue({
-	el: '#root'
+	el: '#root',
+	data: {
+		showModal: false
+	}
 });
